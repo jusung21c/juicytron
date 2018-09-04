96e2818daabbc6395ba98deecfb4fcd4ab1d164d
JuicyJusung <jusung21c@gmail.com>
2018-09-04 09:57:46 +0900

    Home 화면 코드 리팩토링

--- src/renderer/components/Home.vue
+++ src/renderer/components/Home.vue
@@ -1,84 +1,39 @@
 <template>
-    <v-card>
+
+
         <v-container
                 fluid
-                grid-list-lg
+                grid-list-xl
+                text-xs-center
         >
             <v-layout row wrap>
-                <v-flex xs12>
-                    <v-card color="blue-grey darken-2" class="white--text">
-                        <v-card-title primary-title>
-                            <div class="headline">Team Google Drive</div>
+                <v-flex xs8 offset-xs2 v-for="item in items">
+                    <v-card :color="item.color" class="white--text" @click.native="open(item.to)">
+                        <v-card-title primary-title >
+                            <div class="headline">{{item.title}}</div>
                         </v-card-title>
                         <v-card-actions>
                             <v-spacer></v-spacer>
-                            NLP개발팀 구글 드라이브
-                        </v-card-actions>
-                    </v-card>
-                </v-flex>
-
-                <v-flex xs12>
-                    <v-card color="cyan darken-2" class="white--text">
-                        <v-layout>
-                            <v-flex xs7>
-                                <v-card-title primary-title>
-                                    <div>
-                                        <div class="headline">Supermodel</div>
-                                        <div>Foster the People</div>
-                                        <div>(2014)</div>
-                                    </div>
-                                </v-card-title>
-                            </v-flex>
-                        </v-layout>
-                        <v-divider light></v-divider>
-                        <v-card-actions class="pa-3">
-
-                        </v-card-actions>
-                    </v-card>
-                </v-flex>
-
-                <v-flex xs12>
-                    <v-card color="purple" class="white--text">
-                        <v-layout row>
-                            <v-flex xs7>
-                                <v-card-title primary-title>
-                                    <div>
-                                        <div class="headline">Halycon Days</div>
-                                        <div>Ellie Goulding</div>
-                                        <div>(2013)</div>
-                                    </div>
-                                </v-card-title>
-                            </v-flex>
-                            <v-flex xs5>
-                                <v-img
-                                        src="https://cdn.vuetifyjs.com/images/cards/halcyon.png"
-                                        height="125px"
-                                        contain
-                                ></v-img>
-                            </v-flex>
-                        </v-layout>
-                        <v-divider light></v-divider>
-                        <v-card-actions class="pa-3">
-                            Rate this album
-                            <v-spacer></v-spacer>
-                            <v-icon>star_border</v-icon>
-                            <v-icon>star_border</v-icon>
-                            <v-icon>star_border</v-icon>
-                            <v-icon>star_border</v-icon>
-                            <v-icon>star_border</v-icon>
+                            {{item.description}}
                         </v-card-actions>
                     </v-card>
                 </v-flex>
             </v-layout>
         </v-container>
-    </v-card>
 </template>
 
 <script>
 
   export default {
     name: 'welcome',
+    data: () => ({
+      items: [
+        { title: 'Team Google Drive', description: 'NLP개발팀 구글 드라이브', color: 'blue', to: 'https://drive.google.com/drive/folders/16ylgC5pCWVyQIowfwjLDVfZPbSPzYvks?usp=sharing' },
+        { title: 'Manual', description: '메뉴얼', color: 'green', to: 'https://sites.google.com/view/mediazen' },
+        { title: 'Git Server', description: 'gitea server for LP', color: 'purple', to: 'http://58.225.115.230:63000/' },
 
+      ],
+    }),
     methods: {
       open(link) {
         this.$electron.shell.openExternal(link);
